d96b3c9a2a96a675e3473ed5679b70ff5b66864e
Stanislau Komar <stanislau_komar@epam.com>
2023-01-22 03:47:46 +0400

    v1.6.0 released

--- src/ConsolePlugin.php
+++ src/ConsolePlugin.php
@@ -1,25 +1,115 @@
 <?php
 
+/*
+ *  This file is part of the Micro framework package.
+ *
+ *  (c) Stanislau Komar <kost@micro-php.net>
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ */
+
 namespace Micro\Plugin\Console;
 
+use Micro\Component\DependencyInjection\Autowire\AutowireHelperFactory;
+use Micro\Component\DependencyInjection\Autowire\AutowireHelperFactoryInterface;
 use Micro\Component\DependencyInjection\Container;
-use Micro\Framework\Kernel\Plugin\AbstractPlugin;
-use Micro\Plugin\Console\Impl\ConsoleApplicationFacade;
+use Micro\Framework\Kernel\Plugin\DependencyProviderInterface;
+use Micro\Plugin\Console\Business\Factory\ConsoleApplicationFactory;
+use Micro\Plugin\Console\Business\Factory\ConsoleApplicationFactoryInterface;
+use Micro\Plugin\Console\Facade\ConsoleApplicationFacade;
+use Micro\Plugin\Console\Facade\ConsoleApplicationFacadeInterface;
+use Micro\Plugin\Locator\Facade\LocatorFacadeInterface;
 
-class ConsolePlugin extends AbstractPlugin
+/**
+ * A plugin that allows you to provide a console interface for executing commands.
+ *
+ * All application classes that inherit "Symfony\Component\Console\Command\Command" will be automatically found and, if necessary, all dependencies will be automatically provided to the constructor.
+ *
+ * The plugin is an adapter for the <a href="https://symfony.com/doc/current/components/console.html#learn-more" target="_blank">symfony/console</a> component.
+ *
+ * **Installation**
+ * Install plugin
+ * ```bash
+ * $ composer require micro/plugin-console
+ * ```
+ *
+ * Enable plugin
+ * ```php
+ * // /etc/plugins.php
+ *
+ * return [
+ *      // ...other bundles
+ *      Micro\Plugin\Console\ConsolePlugin::class
+ * ];
+ * ```
+ * Register command
+ * ```php
+ *
+ * use Symfony\Component\Console\Command\Command;
+ * use Symfony\Component\Console\Input\InputInterface;
+ * use Symfony\Component\Console\Output\OutputInterface;
+ *
+ * class MyTestCommand extends Command
+ * {
+ *      public function __construct(
+ *          private readonly SomeDependentFacadeInterface $dependentService
+ *      )
+ *      {
+ *          parent::__construct('test:command');
+ *      }
+ *
+ *      public function configure()
+ *      {
+ *          // Configure some options and arguments
+ *      }
+ *
+ *      public function execute(InputInterface $input, OutputInterface $output)
+ *      {
+ *          $result = $this->dependentService->doSomething($input->getArgument('some-configured-argument'));
+ *
+ *          $output->writeln('Result: ' . $result);
+ *
+ *          return self::SUCCESS;
+ *      }
+ * }
+ * ```
+ *
+ *  Execute
+ * ```bash
+ * $ php bin/console test:command
+ * ```
+ *
+ * @author Stanislau Komar <head.trackingsoft@gmail.com>
+ *
+ * @api
+ */
+class ConsolePlugin implements DependencyProviderInterface
 {
-    private readonly Container $container;
-    /**
-     * {@inheritDoc}
-     */
+    private LocatorFacadeInterface $locatorFacade;
+
+    private AutowireHelperFactoryInterface $autowireHelperFactory;
+
     public function provideDependencies(Container $container): void
     {
-        $this->container = $container;
-
         $container->register(
-            ConsoleApplicationFacadeInterface::class, function (Container $container) {
-                return new ConsoleApplicationFacade($container);
+            ConsoleApplicationFacadeInterface::class, function (
+                LocatorFacadeInterface $locatorFacade,
+                Container $container
+            ): ConsoleApplicationFacadeInterface {
+                $this->locatorFacade = $locatorFacade;
+                $this->autowireHelperFactory = new AutowireHelperFactory($container);
+
+                return new ConsoleApplicationFacade($this->createConsoleApplicationFactory());
             }
+        );
+    }
+
+    protected function createConsoleApplicationFactory(): ConsoleApplicationFactoryInterface
+    {
+        return new ConsoleApplicationFactory(
+            $this->locatorFacade,
+            $this->autowireHelperFactory->create()
         );
     }
 }
